689141023e049cb603c32c2a6ada7318beb53e89
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-03-13 21:55:30 +0100

    images sources changed

--- backgroundData.js
+++ backgroundData.js
@@ -1,5 +1,5 @@
 export const backgrounds=[
-    "/images/glowna_Dopalacze1_2x.jpg",
-    "/images/grzyby1_tlo2.png",
-    "/images/film.png"
+    "/images2/app1/glowna_Dopalacze1_2x.jpg",
+    "/images2/app1/grzyby1_tlo2.png",
+    "/images2/app1/film.png"
 ]
--- planszeAdane.js
+++ planszeAdane.js
@@ -1,9 +1,9 @@
 
 export const planszeA = [
-    "/images/grzyby_app.jpg",
-    "/images/grzyby_app2.jpg",
-    "/images/grzyby_app3.jpg",
-    "/images/grzyby_app4.jpg",
-    "/images/grzyby_app5.jpg",
-    "/images/grzyby_app6.jpg"
+    "/images2/app1/grzyby_app.jpg",
+    "/images2/app1/grzyby_app2.jpg",
+    "/images2/app1/grzyby_app3.jpg",
+    "/images2/app1/grzyby_app4.jpg",
+    "/images2/app1/grzyby_app5.jpg",
+    "/images2/app1/grzyby_app6.jpg"
 ]
